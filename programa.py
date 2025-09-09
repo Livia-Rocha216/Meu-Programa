07edde669f2c10a5a65abe21dd3e4617ec9ecfa4
Lívia Rocha <liviaantenucci@colegiopaulodetarso.com.br>
2025-09-09 18:32:06 +0000

    aa

---
+++ programa.py
@@ -0,0 +1,4 @@
+# Programa Interativo: Jogo de Perguntas 
+
+nome = input("Seja bem-vindo ao jogo de perguntas aleatórias que não vão agregar em nada! Qual seu nome?")
+print("Muito bem,"+ nome +" vamos começar o jogo!")
